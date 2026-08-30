2ff603302dac56132297f7c914ed896d56f4ff41
Entkenntnis <Entkenntnis@users.noreply.github.com>
2026-08-30 14:25:27 +0200

    first serious quest

--- src/components/pages/Overview.tsx
+++ src/components/pages/Overview.tsx
@@ -9,16 +9,16 @@ export function Overview() {
   const core = useCore()
 
   return (
-    <div>
-      <h1>JavaQuest</h1>
+    <div className="">
+      <h1 className="mb-6 text-xl ml-4 mt-4">JavaQuest</h1>
       {questsList.map((quest) => {
         const path = `/quest-${quest.id}`
         return (
-          <div>
-            {quest.title}{' '}
+          <div className="ml-4 my-6">
+            [{quest.id}] {quest.title}{' '}
             <a
               href={path}
-              className="px-2 py-0.5 bg-gray-100 hover:bg-gray-200"
+              className="px-2 py-0.5 bg-gray-100 hover:bg-gray-200 rounded"
               onClick={(e) => {
                 e.preventDefault()
                 navigate(core, path)
--- src/lib/data/questsData.ts
+++ src/lib/data/questsData.ts
@@ -3,8 +3,21 @@ import type { QuestData } from '../state/types'
 export const questsData: { [key: number]: QuestData } = {
   1: {
     id: 1,
-    title: 'Triff eine Entscheidung!',
-    code: 'class Program {\n  public static void main(String[] args) {\n    System.out.println(___placeholder___);\n  }\n}',
+    title: 'Willkommen',
+    code: `
+public class Willkommen {
+
+    public void dasIstMeineLieblingszahl(int zahl) {
+        if (zahl == 67) {
+            System.out.println("Geh in die Ecke und schäm dich.");
+        }
+        if (___placeholder___) {
+            System.out.println("Cool, das ist die Antwort auf das Leben!");
+            // Die Antwort ist das Ergebnis von 6 + 4 * 9
+        }
+    }
+}
+    `.trim(),
   },
   2: {
     id: 2,
